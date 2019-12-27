ec04aa69b2d20702e17f723ffedfef83d18e61a7
Mate Papp <matepapp@icloud.com>
2019-12-27 18:15:17 +0100

    Make navbar mobile friendly

--- gatsby/src/components/NavBar.js
+++ gatsby/src/components/NavBar.js
@@ -36,31 +36,51 @@ export const NavBar = () => {
         </div>
       </div>
 
-      <nav
-        className={`${
-          isOpen ? "block" : "hidden"
-        } px-2 pt-2 pb-4 sm:flex sm:p-0`}
-      >
-        <a
-          className="block px-2 py-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-          href="#"
-        >
-          List your property
-        </a>
-        <a
-          className="block px-2 py-1 mt-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-          href="#"
-        >
-          Trips
-        </a>
-        <a
-          className="block px-2 py-1 mt-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
-          href="#"
-        >
-          Messages
-        </a>
-        <div className="ml-6">
-          <Dropdown />
+      <nav className={`${isOpen ? "block" : "hidden"} sm:block`}>
+        <div className="px-2 pt-2 pb-4 sm:flex sm:p-0">
+          <a
+            className="block px-2 py-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+            href="#"
+          >
+            List your property
+          </a>
+          <a
+            className="block px-2 py-1 mt-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+            href="#"
+          >
+            Trips
+          </a>
+          <a
+            className="block px-2 py-1 mt-1 font-semibold text-white rounded hover:bg-gray-800 sm:mt-0 sm:ml-2"
+            href="#"
+          >
+            Messages
+          </a>
+          <div className="hidden sm:block sm:ml-6">
+            <Dropdown />
+          </div>
+        </div>
+        <div className="px-4 py-5 border-t border-gray-800 sm:hidden">
+          <div className="flex items-center">
+            <img
+              src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=256&q=80"
+              alt="Your avatar"
+              className="object-cover w-8 h-8 border-2 border-gray-600 rounded-full"
+              onClick={() => setOpen(prev => !prev)}
+            />
+            <span className="ml-3 font-semibold text-white">Jane Doe</span>
+          </div>
+          <div className="mt-4">
+            <a href="#" className="block text-gray-400 hover:text-white">
+              Account settings
+            </a>
+            <a href="#" className="block mt-2 text-gray-400 hover:text-white">
+              Support
+            </a>
+            <a href="#" className="block mt-2 text-gray-400 hover:text-white">
+              Sign out
+            </a>
+          </div>
         </div>
       </nav>
     </header>
